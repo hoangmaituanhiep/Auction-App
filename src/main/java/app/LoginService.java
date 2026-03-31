caf6d7e0aaaa7bcf9197512c65f994347ed6f8a4
Heip <hoangmaituanhiep@gmail.com>
2026-03-31 12:44:00 +0700

    Update: rename database variable for clearer comprehension

--- src/main/java/app/LoginService.java
+++ src/main/java/app/LoginService.java
@@ -3,12 +3,12 @@ package app;
 import java.sql.*;
 
 public class LoginService {
-    private final String dbUrl = "jdbc:sqlite::resources:database/users.db";
+    private final String databaseUrl = "jdbc:sqlite::resources:database/users.db";
 
     public boolen authenticate(String username, String password) {
         String query = "SELECT password FROM users WHERE username = ?";
 
-        try(Connection connection = DriverManager.getConnection(dbUrl);
+        try(Connection connection = DriverManager.getConnection(databaseUrl);
         Statement statement = connection.prepareStatement(query)) {
             statement.setString(1, username);
             ResultSet result = statement.executeQuery();
