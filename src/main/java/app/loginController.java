2c56bcf772ee41dbb5f98687372a8f53307e825c
Heip <hoangmaituanhiep@gmail.com>
2026-03-31 11:48:10 +0700

    Reorganize folder structure

---
+++ src/main/java/app/loginController.java
@@ -0,0 +1,27 @@
+package app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+
+class LoginService {
+
+}
+
+public class loginController {
+    @FXML private TextField getUserName;
+    @FXML private PasswordField getPassword;
+    @FXML private Label status;
+
+    private LoginService service = new LoginService();
+
+    @FXML
+    private void handleLogin() {
+        if(service.authenticate(getUserName.getText(), getPassword.getText())) {
+            status.setText("Successfull!");
+        }
+        else {
+            status.setText("Failed to login.");
+        }
+    }
+}
