86537f647bfe3a36ccdb26fd62633eeb491999ae
MaiVanTanPhat <25021928@vnu.edu.vn>
2026-05-06 10:29:14 +0700

    thêm controller ccho màn hình ItemInfo

---
+++ src/main/java/app/controllers/SellingController.java
@@ -0,0 +1,12 @@
+package app.controllers;
+
+import java.io.IOException;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class SellingController {
+    private static final Logger logger = LoggerFactory.getLogger(MainWebController.class);
+    @FXML
+    
+}
