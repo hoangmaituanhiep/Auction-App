ee2bbc1534b902ad0f28880d2f8501fd79c43e9d
hoangmaituanhiep <hoangmaituanhiep@gmail.com>
2026-06-02 12:21:48 +0700

    Fix: Redundant connect method

--- Client/src/main/java/app/controllers/MainWebController.java
+++ Client/src/main/java/app/controllers/MainWebController.java
@@ -206,13 +206,6 @@ public class MainWebController {
     }
 
     networkClient = NetworkClient.getInstance();
-    try {
-      networkClient.connect(MainApp.getHost(), MainApp.getPort());
-      logger.info("INFO: Connected to server successfully at: {}:{}", MainApp.getHost(), MainApp.getPort());
-    } catch (IOException e) {
-      logger.error("ERROR: Cannot connect to server. {}", e.getMessage());
-      showAlert("Connection Error", "Cannot connect to server:\n" + e.getMessage());
-    }
 
     networkClient.addUIListener(Message.WELCOME, packet -> {
       logger.info("INFO: Welcome bro.");
