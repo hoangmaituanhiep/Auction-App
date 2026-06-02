1f76bf00b385b88e5a2f8012b03fbc529e5b1f32
hoangmaituanhiep <hoangmaituanhiep@gmail.com>
2026-06-02 10:02:07 +0700

    Fix: Modify test file

--- Client/src/test/java/app/controllers/MainWebControllerTest.java
+++ Client/src/test/java/app/controllers/MainWebControllerTest.java
@@ -12,12 +12,10 @@ import org.junit.jupiter.api.Test;
 import app.functions.Admin;
 import app.functions.User;
 import javafx.application.Platform;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 
@@ -40,7 +38,6 @@ class MainWebControllerTest {
         setField(controller, "logIn", new Button());
         setField(controller, "join", new Button());
         setField(controller, "New", new Button());
-        setField(controller, "searchItems", new TextField());
         setField(controller, "auctionScrollPane", new ScrollPane());
         setField(controller, "logInLabel", new Label());
     }
@@ -56,7 +53,6 @@ class MainWebControllerTest {
         setField(controller, "logInLabel", new Label());
         setField(controller, "join", new Button());
         setField(controller, "New", new Button());
-        setField(controller, "searchItems", new TextField());
         setField(controller, "auctionScrollPane", new ScrollPane());
         setField(controller, "logIn", new Button());
 
@@ -65,7 +61,6 @@ class MainWebControllerTest {
         Button logIn = (Button) getField(controller, "logIn");
         Button join = (Button) getField(controller, "join");
         Button newButton = (Button) getField(controller, "New");
-        TextField searchItems = (TextField) getField(controller, "searchItems");
         ScrollPane auctionScrollPane = (ScrollPane) getField(controller, "auctionScrollPane");
         Label logInLabel = (Label) getField(controller, "logInLabel");
 
@@ -73,7 +68,6 @@ class MainWebControllerTest {
         assertFalse(logIn.isManaged());
         assertFalse(join.isDisable());
         assertFalse(newButton.isDisable());
-        assertFalse(searchItems.isDisable());
         assertFalse(auctionScrollPane.isDisable());
         assertTrue(auctionScrollPane.isVisible());
         assertEquals("Hi admin1", logInLabel.getText());
